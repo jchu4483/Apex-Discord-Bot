6bc53f74ab85f66944fbd51114e41dc561fab877
jchu4483 <jchu4483@gmail.com>
2019-05-24 22:01:41 -0400

    Initial set-up

---
+++ index.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const axios = require('axios');
+require('dotenv').config();
+const client = new Discord.Client();
+
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', (msg) => {
+	if (msg.content.includes('!apexbot')) {
+		const playerName = msg.content.split(" ")[1] || null;
+		if (playerName) {
+			axios.get(`https://www.apexlegendsapi.com/api/v1/player?platform=pc&name=${playerName}`, {
+						headers: {
+							'Authorization': process.env.APEX_API_TOKEN
+						}
+			})
+			.then((res) => {
+				if (res.status === 200) {
+					showMostRecentStats(res, msg, playerName)
+				}
+			})
+			.catch((error) => {
+				console.log(error.response)
+			})
+		}
+	}
+})
+
+function showMostRecentStats(res, msg, playerName) {
+	const legends = res.data.legends;
+	const embed = new Discord.RichEmbed();
+
+	legends.forEach((legend, i) => {
+		legend.stats.forEach((legendStat) => {
+			for (var stat in legendStat) {
+				if (legendStat.hasOwnProperty(stat)) {
+					embed.addField(`Hero`, `${legend.name}`, true)
+					embed.addField(`${stat}`, `${legendStat[stat]}`, true);
+					embed.setColor('#f50057');
+					embed.setDescription(`Here are the stats for ${playerName}`)
+				}
+			}
+		})
+	})
+	msg.channel.send(embed);
+}
+
+client.login(process.env.DISCORD_TOKEN);
